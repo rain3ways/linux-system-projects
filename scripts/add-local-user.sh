9de1d42dee53a27dd8fea3e980b0fa77d3ac311c
Rain <rain3ways@users.noreply.github.com>
2025-11-30 05:57:21 +0000

    feat(add-local-user): improve user validation and password expiry
    - Require running the script as root/sudo
    - Add the -c comment field when creating the user
    - Fail loudly when user or password creation fails
    - Use `passwd -e` to force password change on first login

--- scripts/add-local-user.sh
+++ scripts/add-local-user.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
 # Make sure the script is being executed with super privilleges.
+if [[ $UID -ne 0 ]]; then
+	echo 'Pleas run with sudo or as root' 
+	exit 1
+fi
 # Get the username (login).
 read -p "Enter the username to create: " USER_NAME
 
 # Get the real name (contents for the description field).
-read -p "Enter the name of the person or application that will using this account: " DESCRIPTION
+read -p "Enter the name of the person or application that will using this account: " COMMENT 
 
 # Get the password.
 read -p "Enter the password to use for the account: " PASSWORD
 
 # Create the user with the password.
-useradd -m ${USER_NAME} 
+useradd -c "${COMMENT}" -m ${USER_NAME} 
 
 # Check to see if the useradd command succeeded.
-if [[ $? == 0 ]]; then
-	echo "Changing password for user ${USER_NAME}"
+if [[ $? -ne 0 ]] 
+then
+	echo 'The account could not be created.'
+	exit 1
 fi
 
 # Set the password.
 echo "${USER_NAME}:${PASSWORD}" | chpasswd
 # Check to see if the passwd command succeeded.
-if [[ $? == 0 ]]; then
-	echo "passwd: all authentication tokens updated successfully."
+if [[ $? -ne 0 ]]; then
+	echo 'The password for the account could not be set.'
+	exit 1
 fi
 
 # Force password change on first login.
-echo "Expiring password for user ${USER_NAME}"
-
-passwd ${USER_NAME}
+passwd -e ${USER_NAME}
 
 # Display the username, password, and the host where the user was created.
 echo "username: ${USER_NAME}"
 
 echo "password: ${PASSWORD}"
 
-echo "host: "
+echo "host: ${HOSTNAME}"
