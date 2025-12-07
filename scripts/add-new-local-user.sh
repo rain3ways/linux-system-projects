b34f931b279e060a387ad83dc4fa6e8e6298ebdf
Rain <rain3ways@users.noreply.github.com>
2025-12-07 13:55:02 +0000

    feat(user-management): validate root privileges and arguments in add-new-local-user

--- scripts/add-new-local-user.sh
+++ scripts/add-new-local-user.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # Make sure the script is being executed with superuser privileges.
-
+if [[ ${UID} -ne 0 ]]
+then
+	echo 'Please run with sudo or as root'
+	exit 1
+fi
 # If the user doesn't supply at least one argument, then give them help.
-
-# The first is the use name.
+if [[ $# -lt 1 ]]
+then
+	echo "Usage: $0 USER_NAME [COMMENT] ..."
+	echo "Create a local user with the specified USER_NAME and optional COMMENT."
+	exit 1
+fi
+# The first is the user name.
 
 # The rest of the parameters are for the account comments.
 
