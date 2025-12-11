f2ebc976fae604a8bd87775e471215f0a9692777
Rain <rain3ways@users.noreply.github.com>
2025-12-11 10:38:36 +0000

    feat(linux-conventions): practice stdin stdout stderr in add-newer-local-user script

---
+++ scripts/add-newer-local-user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Make sure the script is being executed with superuser privileges.
+
+# If the user doesn't supply at least one argument, then give them help.
+
+# The first parameter is the user name.
+
+# The rest of the paremeters are for the account comments.
+
+# Generate a password.
+
+# Create the user with the password.
+
+# Check to see if the passwd command succeeded.
+
+# Force password change on first login.
+
+# Display the username, password, and the host where the user was created.
+
+
