37990595f982dd79de6366c982c0df06de51a32f
Rain <rain3ways@users.noreply.github.com>
2025-12-27 12:14:41 +0700

    chore(functions): extend log demo and add backup_file skeleton

--- scripts/local-user-demo-10.sh
+++ scripts/local-user-demo-10.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 log() {	
+	# This function sends a message to syslog and to standard output if VERBOSE is true.
 	local MESSAGE="$@"
 	if [[ "${VERBOSE}" = "True" ]]
 	then
 		echo "${MESSAGE}"
 	fi
+	logger -t local-user-demo-10.sh "${MESSAGE}"
 }
+
+backup_file(){
+	# This function creates a backup of a file. Return non-zero status on error.
+	
+	local FILE="${1}"
+	# Make sure the file exists.
+	if [[ -f "${FILE}" ]]
+	then
+		local BACKUP_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+	fi
+}
+
 readonly VERBOSE='True'
 log 'True' 'Hello!'
 log 'This is fun!'
