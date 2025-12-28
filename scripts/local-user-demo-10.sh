6cf23492da1b99e47338f0e9c45f9115a4fb7bd9
Rain <rain3ways@users.noreply.github.com>
2025-12-28 14:35:02 +0700

    feat(functions): implement log and backup_file helpers for file backup

--- scripts/local-user-demo-10.sh
+++ scripts/local-user-demo-10.sh
@@ -18,9 +18,27 @@ backup_file(){
 	if [[ -f "${FILE}" ]]
 	then
 		local BACKUP_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+		log "Backing up ${FILE} to local ${BACKUP_FILE}."
+
+		# The status of the funciton will be the exit status of the cp command.
+		cp -p ${FILE} ${BACKUP_FILE}
+	else
+		# The file does not exist, so return a non-zero exit status.
+		return 1
+	fi
+
+	# Make a decision based on the exit status of the function.
+	if [[ ${?} -eq 0 ]]
+	then
+		echo 'File backup succeeded!'
+	else
+		echo 'File backup failed!'
 	fi
 }
 
 readonly VERBOSE='True'
+
 log 'True' 'Hello!'
 log 'This is fun!'
+
+backup_file '/etc/passwd'
